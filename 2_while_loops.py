dbc444a96ebfb473281d5f9cdd9b0552c1997e5c
Flipout23 <whenlimit11@gmail.com>
2025-12-09 15:22:41 +0000

    while loops

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,4 +1,48 @@
 #notes on while loops
+#while loop = execute some code WHILE some condition remains true
+
+#(if)
+# name = input("Enter your name")
+
+# if name == "":
+#     print("You did not enter your name")
+# else:
+#     print(f"hello {name}")
+
+#(while)
+# name = input("Enter your name:")
+
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name:")
+
+# print(f"Hello {name}")
+
+#(age)
+# age = int(input("Enter your age: "))
+
+# while age < 0:
+#     print("Age can't be negative")
+#     age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old.")
+
+#food
+# food = input("Enter a food you like (q to quit):")
+
+# while not food == "q":
+#     print(f"You like {food}")
+#     food = input("Enter another food you like (q to quit): ")
+
+# print("bye")
+
+# num = int(input("Enter a number between 1-10: "))
+
+# while num < 1 or num > 10:
+#     print(f"{num} is not valid")
+#     num = int(input("Enter a number between 1-10: "))
+
+# print(f"Your number is: {num}")
 
 # Given:
 colors = ["red", "blue", "green", "yellow", "purple"]
