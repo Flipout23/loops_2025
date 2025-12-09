7bf8b7c4d8659c85a2ed0b5ad0fff832c16d1ac8
Flipout23 <whenlimit11@gmail.com>
2025-12-09 15:29:47 +0000

    while

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -50,3 +50,9 @@ colors = ["red", "blue", "green", "yellow", "purple"]
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+index = 0
+while index < len(colors):
+    if colors[index] == "yellow":
+        break
+    print(colors[index])
+    index += 1 #increment the index to avoid infinite loop
