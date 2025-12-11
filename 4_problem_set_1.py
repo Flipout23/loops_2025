ef25d219bac8a3d163c743008eadc1a8b727abd1
Flipout23 <whenlimit11@gmail.com>
2025-12-11 14:36:31 +0000

    problemset

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -5,50 +5,46 @@
 
 
 # ### **Problem 1: Print Numbers 1 to 10
-
 # Write a program that prints the numbers from **1 to 10**, each on a new line.
-
+list1to10 = list(range(1,11))
+for number in list1to10:
+    print(number)
 
 
 # ### **Problem 2: Sum of Numbers
-
 # Ask the user for a number **n**, then calculate and display the **sum of all numbers from 1 to n**.
-
+n = int(input("enter a number: "))
+for number in range(1, n + 1):
+    total_sum += number
+print("the sum of numbers from 1 to", n, "is:", total_sum)
 
 
 # ### **Problem 3: Factorial Calculator
-
 # Ask the user for a number **n**, then calculate the **factorial** of that number.
-
 # *(Example: factorial of 5 is 120)
 
 
 # ### **Problem 4: Count Vowels**
-
 # Ask the user for a string. Count and print how many **vowels (a, e, i, o, u)** are in the string.
 
 
 # ### **Problem 5: Print Even Numbers**
-
 # Ask the user for a number **n**, then print all **even numbers** from 2 up to n.
 
 
 
 # ### **Problem 6: Reverse a String**
-
 # Ask the user for a string, then print the string **backwards**.
 
 
 
 # ### **Problem 7: Multiplication Table**
-
 # Ask the user for a number **n**, then print the **multiplication table** for n from
 # n × 1 up to n × 10.
 
 
 
 # ### **Problem 8: Count Occurrences**
-
 # Ask the user for **two strings**, a and b.
 # Determine how many times **string b appears inside string a**.
 
